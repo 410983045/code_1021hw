49edcc0a228d2b6057f371d09bc212afe84fcd5b
410983045 <71751207+410983045@users.noreply.github.com>
2020-10-27 23:10:38 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    for (let y = 0; y <= 8; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(4 - x, 4 - (y - x))
+            basic.pause(100)
+        }
+    }
+    basic.clearScreen()
+})
