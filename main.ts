d92d5ce0f9db538e3ec0bce523400c9db1e18070
410983045 <71751207+410983045@users.noreply.github.com>
2020-11-04 14:19:11 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,13 @@ basic.forever(function () {
     for (let y = 0; y <= 8; y++) {
         for (let x = 0; x <= 4; x++) {
             led.plot(4 - x, 4 - (y - x))
-            basic.pause(100)
         }
+        basic.pause(100)
     }
-    basic.clearScreen()
+    for (let y = 0; y <= 8; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.unplot(x, y - x)
+        }
+        basic.pause(100)
+    }
 })
